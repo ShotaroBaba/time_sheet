ec18bfd547b98ab03b804ea338c40d08b40258c8
ShotaroBaba <bshotaro@gmail.com>
2021-04-27 23:00:41 +0900

    Added sql code for storing a employee's time (Fixed a decryption script file name).

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,8 +1,13 @@
 -- A table for storing a user's time sheets. This records amounts of employees' time which
 -- they spend on doing their job.
 -- TOD: All tables needs to be encrypted.
+START TRANSACTION;
+CREATE DATABASE IF NOT EXISTS `time_sheet` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `time_sheet`;
 
-CREATE TABLE `user` (
+
+
+CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int PRIMARY KEY,
   `firstname` varchar(255) NOT NULL,
   `middlename` varchar(255),
@@ -11,7 +16,7 @@ CREATE TABLE `user` (
   `email` varchar(255) NOT NULL
 );
 
-CREATE TABLE `time_sheet` (
+CREATE TABLE IF NOT EXISTS `time_sheet` (
   `time_id` int PRIMARY KEY,
   `user_id` int NOT NULL,
   `time` datetime NOT NULL,
@@ -19,7 +24,7 @@ CREATE TABLE `time_sheet` (
 );
 
 -- Different salt is created for different users.
-CREATE TABLE `user_password` (
+CREATE TABLE IF NOT EXISTS `user_password` (
   `user_id` int,
   `salt` varchar(50) NOT NULL,
   `password` varchar(256) NOT NULL
@@ -28,3 +33,7 @@ CREATE TABLE `user_password` (
 ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `user_password` (`user_id`);
 
 ALTER TABLE `time_sheet` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+GRANT SELECT, INSERT ON `time_sheet`.* TO 'time_sheet_manager'@'%';
+
+COMMIT;
