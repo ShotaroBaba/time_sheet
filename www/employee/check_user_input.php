4830b68bc2566124e40706347ce633b75bb6be2c
ShotaroBaba <bshotaro@gmail.com>
2021-05-06 23:54:06 +0900

    Added SQL for detecting the duplication of user name and phone number on  table.

--- www/employee/check_user_input.php
+++ www/employee/check_user_input.php
@@ -24,7 +24,7 @@ try {
   `email` = :_email;");
 
   // $check_email_address_prepare->bindValue(":_email", htmlspecialchars($_POST['employeeEmail']), PDO::PARAM_STR);
-  $check_email_address_prepare->bindValue(":_email", "fwefaefwae", PDO::PARAM_STR);
+  $check_email_address_prepare->bindValue(":_email", $_POST['employeeEmail'], PDO::PARAM_STR);
 
   $check_email_address_result=$check_email_address_prepare->execute();
 
@@ -34,7 +34,28 @@ try {
     $check_email_address_prepare=NULL;
   }
   
-  $check_email_address_prepare=NULL;
+  $check_user_name_phone_prepare=
+  $conn->prepare("SELECT COUNT(*) as user_name_phone_count
+  FROM `user` WHERE
+  `first_name` = :_first_name AND
+  `middle_name` = :_middle_name AND
+  `last_name` = :_last_name AND 
+  `phone_number` = :_phone_number ;");
+
+  $check_user_name_phone_prepare->bindValue(":_first_name", $_POST['employeeFirstName'], PDO::PARAM_STR);
+  $check_user_name_phone_prepare->bindValue(":_middle_name", $_POST['employeeMiddleName'], PDO::PARAM_STR);
+  $check_user_name_phone_prepare->bindValue(":_last_name", $_POST['employeeLastName'], PDO::PARAM_STR);
+  $check_user_name_phone_prepare->bindValue(":_phone_number", $_POST['employeePhoneNumber'], PDO::PARAM_STR);
+
+  $check_user_name_phone_result=$check_user_name_phone_prepare->execute();
+
+   // Email already exists.
+   if($check_email_address_result > 0){
+    $is_name_phone_exist=true;
+    $check_user_name_phone_prepare=NULL;
+  }
+
+  $check_user_name_phone_prepare=NULL;
   echo "{'email':$is_email_exist,
   'account':$is_name_phone_exist}";
 
