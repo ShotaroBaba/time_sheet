1fa8853e346e838821865ba8c2c042fe184cb2b9
ShotaroBaba <bshotaro@gmail.com>
2021-06-14 10:14:45 +0900

    Fixed '.config.php' file

--- www/.secret/.config_sample.php
+++ www/.secret/.config_sample.php
@@ -8,7 +8,7 @@ $user = 'time_sheet_client';
 // by executing the create_server.sh script.
 $pass   = '_____time_sheet_pass_____';
 $pepper = '_____pepper_string_____';
-$error_message = "Unknown error."
+$error_message = "Unknown error.";
 
 $user_login_expiration_time=1800;
 
